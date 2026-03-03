40a413bc6b3d98f1f871cfc719eca008f967cda7
Marcin Adamowski <marcin@memex.tech>
2026-03-03 09:46:31 +0000

    update header

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ st.set_page_config(
     page_title="Data App", layout="wide", initial_sidebar_state="collapsed"
 )
 
-st.title("Welcome to Memex")
+st.title("Welcome")
 st.markdown("Start building something amazing.")
